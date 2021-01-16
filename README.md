f18688ee8b4009b2b3ea4bb9b5492d43cddc0994
Devejya Raghuvanshi <draghuva@edu.uwaterloo.ca>
2021-01-15 19:11:06 -0500

    Update code for py3.6 and readme

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # Hangman
-Made a Quick Small HangMan game back in highschool to use a few concepts of Object Oriented Programming.
+*Quite Hard* HangMan game played on the Command Line.
 
+Some screenshots of the game
 
+## Game Beginning:
+![Game Start](./Screenshots/GameStart.png)
+
+## Game Lost Screen 😔
+![Lebron James was too hard to guess](./Screenshots/GameLoss1.png)
+![Hints would help alot!](./Screenshots/GameLoss2.png)
+
+## Game Won!!! 🎉
+![Finally](./Screenshots/GameWin.png)
+
+### User Reviews:
+
+> Sucks - My 10 year old brother
+
+> Looks ugly - My girlfriend
+
+> Harder than the impossible quiz, still fun tho 🙃 - Me
+
+### Future improvements:
+
+* Display hints!
+* Increase Number of Words
+* Provide Categories
+* Improve graphics (Currently non existent)
+* Add levels
+* Make a small web app so people can actually play it!
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Devejya Raghuvanshi - 12/A, Program:- Hangman using OOP
 import random  #RANDOM MODULE 
 
 board = [
@@ -17,7 +16,7 @@ class Hangman:
 		self.missed_letters = []
 		self.guessed_letters = []
 		
-        def guess(self,letter): #to compare input letters with letters of the word
+	def guess(self,letter): #to compare input letters with letters of the word
 		if letter in self.word and letter not in self.guessed_letters:
 			self.guessed_letters.append(letter)
 		elif letter not in self.word and letter not in self.missed_letters:
@@ -26,14 +25,14 @@ class Hangman:
 			return False
 		return True
 		
-def hangman_over(self):    # To finish the game. Either by winning or inputing 6 wrong letters
+	def hangman_over(self):    # To finish the game. Either by winning or inputing 6 wrong letters
 		return self.hangman_won() or (len(self.missed_letters) == 6)
-		
-        def hangman_won(self): # defining a function to provide the details on how to win the game.
+			
+	def hangman_won(self): # defining a function to provide the details on how to win the game.
 		if '_' not in self.hide_word():
 			return True
 		return False
-		
+			
 	def hide_word(self):
 		rtn = ''
 		for letter in self.word:
@@ -42,40 +41,33 @@ def hangman_over(self):    # To finish the game. Either by winning or inputing 6
 			else:
 				rtn += letter
 		return rtn
-		
+			
 	def print_game_status(self):
-		print board[len(self.missed_letters)]
-		print 'Word: ' + self.hide_word()
-		print 'Letters Missed: ', 
-		for letter in self.missed_letters:
-			print letter, 
-		print 
-		print 'Letters Guessed: ',
-		for letter in self.guessed_letters:
-			print letter,
-		print 
+		print (board[len(self.missed_letters)])
+		print ('Word: ' + self.hide_word())
+		print ('Letters Missed: ', self.missed_letters)
+		print ('Letters Guessed: ', self.guessed_letters)
 
 def rand_word():
+		bank = ['the','living','pearl','.com','captain','deadbones','chef','rice','pen','lamp','red bottle','random','interstellar','constitution','popular','word','departed','good fellas','mr.bean','jurassic world','hunter','naruto','death note','fetty wap','kendrick lamar','gaurd','cocoa','life','narcos','python','house','mentalist','big bang theory','hangman','crack','butter chicken','martian','superman','hangover','chicago','miami','stuart little','jordan','spalding','manganese','japan','india','paris','cristiano ronaldo','freedom','lebron james','laptop','apple','football','basketball']
+		return bank[random.randint(0,len(bank))]
 	
-	bank = ['the','living','pearl','.com','captain','deadbones','chef','rice','pen','lamp','red bottle','random','interstellar','constitution','popular','word','departed','good fellas','mr.bean','jurassic world','hunter','naruto','death note','fetty wap','kendrick lamar','gaurd','cocoa','life','narcos','python','house','mentalist','big bang theory','hangman','crack','butter chicken','martian','superman','hangover','chicago','miami','stuart little','jordan','spalding','manganese','japan','india','paris','cristiano ronaldo','freedom','lebron james','laptop','apple','football','basketball']
-	return bank[random.randint(0,len(bank))]
-
 def main():
 	
 	game = Hangman(rand_word())
 	while not game.hangman_over():
 		game.print_game_status()
-		user_input = raw_input('\nEnter a letter: ')
+		user_input = input('\nEnter a letter: ')
 		game.guess(user_input)
 
 	game.print_game_status()	
 	if game.hangman_won():
-		print '\nCongratulations! You are the winner of Hangman!'
+		print ('\nCongratulations! You are the winner of Hangman!')
 	else:
-		print '\nSorry, you have lost in the game of Hangman...'
-		print 'The word was ' + game.word
+		print ('\nSorry, you have lost in the game of Hangman...')
+		print ('The word was ' + game.word)
 		
-	print '\nCiao!\n'
+	print ('\nCiao!\n')
 		
 if __name__ == "__main__":
 	main()
